56751cae2959c4223d7489e616e787b7c19a04d1
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-17 22:17:16 +0200

    Trying to get rid of this

--- config.php
+++ config.php
@@ -1,17 +1,13 @@
 <?php
-
 class LgmsConfig {
 
-    const app_title = "SRB Test";
-    const db = "locloud_test";
+    const app_title = "Geocoding Microservice IV";
+    const db = "locloud_test18";
     const db_host = "localhost";
     const db_usr = "root";
     const db_pwd = "root";
-    const basedir = "C:\GitHub\LoCLoud.Geocoding";
-    const app_version = "1.2.27";
+    const basedir = "C:\GitHub\Avinet.LoCloud.Geocoding";
+    const app_version = "1.5.6";
     const updateurl = "http://locloud.avinet.no/latest";
-    const debug = false;
-
+    
 }
-
-?>
