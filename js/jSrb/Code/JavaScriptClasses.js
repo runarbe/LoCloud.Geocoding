9084ac1f5d162a8faecb1ee427ab7aaa9c4869ac
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-17 16:55:07 +0200

    Added UserLevels, AccessLevels and GcMods - the latter an object of JavaScript variables that map to the various modules of the geocoding micro service.

--- js/jSrb/Code/JavaScriptClasses.js
+++ js/jSrb/Code/JavaScriptClasses.js
@@ -1,6 +1,20 @@
 /**
+ * Enumeration for named modules
+ * @class GcMods
+ */
+var GcMods = {
+    mainWindow: "./index.php",
+    manageDatasources: "./view.php?mod=manage-datasources&",
+    manageUsers: "./view.php?mod=manage-users",
+    registerUser: "./view.php?mod=register-user",
+    manageGroups: "./view.php?mod=manage-groups",
+    firstRun: ".firstrun.php",
+    login: "./auth.php?action=login"
+};
+
+/**
  * Enumeration for named web services
- * @class Ws
+ * @class WsUrl
  */
 var WsUrl = {
     getDsAdm0Areas: "./ws/ws-adm0.php",
@@ -9,6 +23,7 @@ var WsUrl = {
     getDsCategories: "./ws/ws-cat.php",
     getLatestVersion: "./ws/ws-check-updates.php",
     createConfig: "./ws/ws-create-config.php",
+    initDb: "./ws/ws-init-db.php",
     handleExcelUpload: "./ws/ws-excel-upload.php",
     getGeoJSON: "./ws/ws-geojson.php",
     getCharacterEncodings: "./ws/ws-get-encodings.php",
@@ -23,9 +38,12 @@ var WsUrl = {
     searchGeonames: "./ws/ws-search-geonames.php",
     updateAttributes: "./ws/ws-update-attrib.php",
     upgradeInstallation: "./ws/ws-upgrade.php",
-    uploadTextFile: "./ws/ws-upload-textfile.php"
+    uploadTextFile: "./ws/ws-upload-textfile.php",
+    manageUsr: "./ws/ws-grd-usr.php",
+    manageDatasources: "./ws/ws-grd-datasources.php",
+    manageDatasourcesACL: "./ws/ws-grd-datasourcesacl.php",
+    manageAcl: "./ws/ws-acl.php"
 };
-
 /**
  * Enumeration for different web service status codes
  * @class WsStatus
@@ -56,21 +74,64 @@ function WsRetObj() {
      * @property Number v
      */
     this.v = WsStatus.success;
-
     /**
      * An object containing named or indexed properties specific to each web
      * service
      * 
      * @property {Object} d
      */
-    this.d = new Object()
-
+    this.d = new Object();
     /**
      * An array of information or error message strings
      * 
      * @property Array m
      */
     this.m = new Array();
-
     return this;
 }
+
+/**
+ * User levels
+ * @class UsrLevel
+ */
+var UsrLevel = {
+    /**
+     * The super administrator can upgrade the application, change and delete all
+     * data sources and change and delete all users
+     * @type Number
+     */
+    "SuperAdmin": 1,
+    
+    /**
+     * The can delete all data sources that he or she
+     * has access to regardless 
+     * @type Number
+     */
+    "Admin": 2,
+    /**
+     * The Editor can edit the access control list of data sources that he or she
+     * has access to
+     * @type Number
+     */
+    "Editor": 4,
+    /**
+     * The User is a regular 
+     * @type Number
+     */
+    "User": 6,
+    /**
+     * This is a placeholder for anonymous users
+     * @type Number
+     */
+    "GuestUser": 99
+};
+
+/**
+ * Access control list levels
+ * @class AccessLevels
+ */
+var AccessLevels = {
+    "Owner": 10,
+    "Editor": 20,
+    "Contributor": 30
+};
