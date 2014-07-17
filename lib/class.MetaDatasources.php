120a3991a7b3991a28f1450211696a1133fdb77e
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-17 17:01:24 +0200

    Major upgrade of table class

--- lib/class.MetaDatasources.php
+++ lib/class.MetaDatasources.php
@@ -1,7 +1,7 @@
 <?php
 
 class MetaDatasources extends MySQLTable {
-    
+
     public $id = null;
     public $ds_title = null;
     public $ds_col_pk = null;
@@ -17,11 +17,61 @@ class MetaDatasources extends MySQLTable {
     public $ds_col_image = null;
     public $ds_col_url = null;
     public $ds_col_puri = null;
-    
+
     public function __construct($pValues = null) {
-        parent::__construct("meta_datasources", "id", $pValues);
-        $this->setMandatory(array("ds_title", "ds_col_pk", "ds_col_name", "ds_srs", "ds_coord_prec"));
+        parent::__construct("meta_datasources",
+                "id",
+                $pValues);
+        $this->_getMandatory(array("ds_title", "ds_col_pk", "ds_col_name", "ds_srs", "ds_coord_prec"));
+
+        $this->_select = sprintf("SELECT DISTINCT SQL_CALC_FOUND_ROWS t.* FROM meta_datasources t INNER JOIN meta_usr_datasources_acl m ON (m.datasource_id = t.id AND m.usr_id=%s AND m.access >= %s)",
+                cUsr(),
+                cLevel());
     }
-    
+
+    /**
+     * Get the field names from the respective datasources
+     * @param int $pDatasourceID The ID of the desired data source
+     * @return array|boolean Array of field names on success, false on error
+     */
+    public function getDatasourceFieldNames($pDatasourceID) {
+        if (Db::tableExists("ds" . $pDatasourceID)) {
+            $mRes = Db::query("SELECT * FROM ds" . $pDatasourceID . " LIMIT 1;");
+            if ($mRes !== false && mysqli_num_rows($mRes) >= 1) {
+                $mReturn = array_keys(mysqli_fetch_assoc($mRes));
+                return $mReturn;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param int $pDatasourceID
+     * @param W2UIRetObj $pRetObj
+     * @return boolean
+     */
+    public static function deleteDatasource($pDatasourceID, &$pRetObj) {
+        $mMetaDatasource = new MetaDatasources();
+        $mMetaDatasource->id = $pDatasourceID;
+        $mMetaDatasource->delete($pRetObj);
+        
+        MetaAcl::DeleteDatasourceAclEntries($pDatasourceID);
+        
+        $mSql = "DELETE FROM meta_datasources WHERE id = $pDatasourceID;
+            DROP TABLE IF EXISTS ds".$pDatasourceID.";
+            DROP TABLE IF EXISTS ds".$pDatasourceID."_cat;
+            DROP TABLE IF EXISTS ds".$pDatasourceID."_adm0;
+            DROP TABLE IF EXISTS ds".$pDatasourceID."_adm1;
+            DROP TABLE IF EXISTS ds".$pDatasourceID."_match;";
+        $mRes = Db::query_multi($mSql);
+        if ($mRes !== false) {
+            $pRetObj->setSuccess("Successfully deleted data source tables");
+            return true;
+        } else {
+            $pRetObj->setFailure("Error deleting data source tables");
+            return false;
+        }
+    }
+
 }
-?>
