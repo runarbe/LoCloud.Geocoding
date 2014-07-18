70c097302ee31a424f91b571c4b1264f0b0ad645
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-18 17:16:28 +0200

    Cleaned up and added comments to the SearchMatch class

--- lib/class.SearchMatch.php
+++ lib/class.SearchMatch.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of SearchMatch
  *
@@ -14,21 +8,51 @@
 class SearchMatch {
     
     /**
-     *
-     * @var type 
+     * The name to be displayed in the result list
+     * @var String 
      */
     public $displayName;
     
+    /**
+     * The actual name (may be shorter, longer)
+     * @var String
+     */
     public $name;
     
+    /**
+     * The longitude of the name in WGS84 geographical coordinates
+     * @var type 
+     */
     public $lng;
-    
+
+    /**
+     * The latitude of the name in WGS84 geographical coordinates
+     * @var type 
+     */
     public $lat;
     
+    /**
+     * Optionally a WKT geometry string that can contain a more complex geometry
+     * @var String (WKT)
+     */
     public $wkt;
     
+    /**
+     * A floating point number between 0 and 100 that expresses the confidence of
+     * the search result
+     * @var Float 
+     */
     public $confidence;
     
+    /**
+     * Returns a new search result
+     * @param String $pName Name
+     * @param String $pDisplayName Display name
+     * @param Double $pLng Longitude
+     * @param Double $pLat Latitude
+     * @param String $pWkt Well-known-text geometry
+     * @param Double $pConfidence Confidence level of match
+     */
     public function __construct($pName = null, $pDisplayName = null, $pLng = null, $pLat = null, $pWkt = null, $pConfidence = 1) {
         if ($pName !== null) {
             $this->name = $pName;
