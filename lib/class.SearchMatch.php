e05632705413205a75a38dcb2e3e373699bb5b70
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-18 18:20:14 +0200

    Rewired the class to include a parameter-less constructor and a static method to return a new instance

--- lib/class.SearchMatch.php
+++ lib/class.SearchMatch.php
@@ -6,19 +6,25 @@
  * @author runarbe
  */
 class SearchMatch {
-    
+
+    /**
+     * Unique identifier of the name within the source
+     * @var String 
+     */
+    public $id;
+
     /**
      * The name to be displayed in the result list
      * @var String 
      */
     public $displayName;
-    
+
     /**
      * The actual name (may be shorter, longer)
      * @var String
      */
     public $name;
-    
+
     /**
      * The longitude of the name in WGS84 geographical coordinates
      * @var type 
@@ -30,22 +36,36 @@ class SearchMatch {
      * @var type 
      */
     public $lat;
-    
+
     /**
      * Optionally a WKT geometry string that can contain a more complex geometry
      * @var String (WKT)
      */
     public $wkt;
-    
+
     /**
      * A floating point number between 0 and 100 that expresses the confidence of
      * the search result
      * @var Float 
      */
     public $confidence;
-    
+
     /**
-     * Returns a new search result
+     * Persistent URI of the name
+     * @var String 
+     */
+    public $pUri;
+
+    /**
+     * Default constructor
+     */
+    public function __construct() {
+        
+    }
+
+    /**
+     * Get a new, populated instance of a SearchMatch class
+     * @param String $pID Identifier of name
      * @param String $pName Name
      * @param String $pDisplayName Display name
      * @param Double $pLng Longitude
@@ -53,29 +73,61 @@ class SearchMatch {
      * @param String $pWkt Well-known-text geometry
      * @param Double $pConfidence Confidence level of match
      */
-    public function __construct($pName = null, $pDisplayName = null, $pLng = null, $pLat = null, $pWkt = null, $pConfidence = 1) {
-        if ($pName !== null) {
-            $this->name = $pName;
-        }
-        
-        if ($pDisplayName !== null) {
-            $this->displayName = $pDisplayName;
-        } else if ($pDisplayName === null && $pName !== null) {
-            $this->name = $pName;
-        }
-        
+    public static function get($pID,
+            $pLng,
+            $pLat,
+            $pName,
+            $pDisplayName = null,
+            $pUri = null,
+            $pWkt = null,
+            $pConfidence = 1) {
+
+        $mSearchMatch = new SearchMatch();
+
+        $mSearchMatch->id = $pID;
+
         if (is_numeric($pLng) && is_numeric($pLat)) {
-            $this->lng = $pLng;
-            $this->lat = $pLat;
+            $mSearchMatch->lng = $pLng;
+            $mSearchMatch->lat = $pLat;
         }
-        
+
+        $mSearchMatch->name = $pName;
+
+        if ($pDisplayName !== null) {
+            $mSearchMatch->displayName = $pDisplayName;
+        } else if ($pDisplayName === null && $pName !== null) {
+            $mSearchMatch->name = $pName;
+        }
+
         if ($pWkt !== null) {
-            $this->wkt = $pWkt;
+            $mSearchMatch->wkt = $pWkt;
         }
-        
+
+        if ($pUri !== null) {
+            $mSearchMatch->uri = $pUri;
+        }
+
         if (is_numeric($pConfidence)) {
-            $this->confidence = $pConfidence;
+            $mSearchMatch->confidence = $pConfidence;
         }
+
+        return $mSearchMatch;
     }
-    
+
+    /**
+     * Check if the name has a WKT set
+     * @return bool True if has WKT, false if not
+     */
+    public function hasWkt() {
+        return $this->wkt !== null ? true : false;
+    }
+
+    /**
+     * Check if the name has a persistent URI set
+     * @return bool True if has persistent URI, false if not
+     */
+    public function hasUri() {
+        return $this->uri !== null ? true : false;
+    }
+
 }
