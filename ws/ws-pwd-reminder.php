f38d0cdb5ce83a60cf383737360b70074e7755b6
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-19 23:17:10 +0200

    Implemented boiler plate template for future web service - requires email address to be added to user profiles.

--- ws/ws-pwd-reminder.php
+++ ws/ws-pwd-reminder.php
@@ -1,7 +1,18 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+require_once('../functions.php');
+
+class WsSendPasswordReminder extends GcWebService implements iWebService {
+
+    protected function _execute() {
+        $this->_result->setFailure("Not implemented");
+        $this->_result->echoJson();
+    }
+
+    public static function getInstance() {
+        return new WsSendPasswordReminder();
+    }
+
+}
+
+WsSendPasswordReminder::getInstance()->run(true);
