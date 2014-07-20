56ff3524a6e4a4308596643a1530a164d55a5c27
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-21 00:02:08 +0200

    Corrected update statement

--- ws/ws-update2.php
+++ ws/ws-update2.php
@@ -15,23 +15,26 @@ class WsUpdateItem extends GcWebService implements iWebService {
                     WsDataTypes::mDouble),
             "y" => new ParamOpt(true,
                     WsDataTypes::mDouble),
+            "name" => new ParamOpt(true,
+                    WsDataTypes::mString),
             "confidence" => new ParamOpt(true,
                     WsDataTypes::mDouble),
             "mapResolution" => new ParamOpt(true,
                     WsDataTypes::mDouble),
+            "geom" => new ParamOpt(false,
+                    WsDataTypes::mString,
+                    null),
             "pURI" => new ParamOpt(false,
                     WsDataTypes::mString,
                     null),
-            "name" => new ParamOpt(true,
-                    WsDataTypes::mString),
             "fieldChanges" => new ParamOpt(false,
                     WsDataTypes::mString)
         );
 
-
         $mP = $this->_getParams($mCheck);
 
         if ($this->_isSuccess()) {
+
 
             if (!isEscaped($mP["name"])) {
                 $mP["name"] = Db::esc($mP["name"]);
@@ -43,26 +46,29 @@ class WsUpdateItem extends GcWebService implements iWebService {
             /*
              * Determine the name of the table to update based on the datasource id and the naming rules
              */
-            $mTableName = "ds" . $mP["dsID"] . "_match";
+            $mTableName = "ds" . $mP["datasourceID"] . "_match";
 
             /*
              * Upsert record into database - user ID + item ID is unique key in the table
              */
-            $mSql = sprintf('INSERT INTO %s (fk_ds_id, gc_name, gc_lon, gc_lat,gc_probability, gc_geom, gc_fieldchanges, gc_usr_id) VALUES (%s, \'%s\', null, %s, %s, %s, \'%s\', \'%s\', %s) ON DUPLICATE KEY UPDATE SET gc_name=\'%s\', gc_lon=%s, gc_lat=%s ,gc_probability=%s, gc_geom=\'%s\', gc_fieldchanges=\'%s\'',
+            $mSql = sprintf('INSERT INTO %s (fk_ds_id, gc_name, gc_lon, gc_lat,gc_probability, gc_geom, gc_fieldchanges, gc_usr_id) VALUES (%s, \'%s\', %s, %s, %s, \'%s\', \'%s\',%s) ON DUPLICATE KEY UPDATE gc_name=\'%s\', gc_lon=%s, gc_lat=%s ,gc_probability=%s, gc_geom=\'%s\', gc_fieldchanges=\'%s\'',
                     $mTableName,
                     $mP["itemID"],
                     $mP["name"],
-                    $mP["lon"],
-                    $mP["lat"],
+                    $mP["x"],
+                    $mP["y"],
                     $mP["confidence"],
+                    $mP["geom"],
                     $mP["fieldChanges"],
                     cUsr(),
                     $mP["name"],
-                    $mP["lon"],
-                    $mP["lat"],
+                    $mP["x"],
+                    $mP["y"],
                     $mP["confidence"],
+                    $mP["geom"],
                     $mP["fieldChanges"]
             );
+            logIt($mSql);
             Db::query($mSql);
             if (mysqli_affected_rows(Db::$conn) >= 1) {
                 $this->_result->setSuccess();
