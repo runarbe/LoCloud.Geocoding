05e89305e96194d221599375af5bdda913cd4ecf
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-20 17:49:56 +0200

    Rearranged form to use PureCSS responsive grids, replaced probability buttons with a numeric input field called confidence. Added a field that contains the current map resolution at the time an object is digitized Added a field that will hold a linked persistent URI Added a button to clear the linked persistent URI

--- mod/mod-form.php
+++ mod/mod-form.php
@@ -1,56 +1,55 @@
 <div id="gcFormContainer" class="rounded ui-state-highlight">
     <div id="gcForm">
-        <form id="gc">
-            <table id="gcFormTable" class="wide">
-                <tr>
-                    <td colspan="4" class="label">
-                        Name of item
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4">
-                        <input id="tbItemName" name="gc_name" class="rounded wide resetable"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="label">X-coordinate (or longitude)</td>
-                    <td class="label">Y-coordinate (or latitude)</td>
-                    <td colspan="2" class="align-right label">Probability (1=certain)</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="text" id="tbLongitude" name="tbLongitude" class="rounded wide resetable"/>
-                    </td>
-                    <td>
-                        <input type="text" id="tbLatitude" name="tbLatitude" class="rounded wide resetable"/>
-                    </td>
-                    <td colspan="2" class="align-right">
-                        <div id="radio">
-                            <input type="radio" id="radio1" name="rbProbability" value="1"/><label for="radio1">1</label>
-                            <input type="radio" id="radio2" name="rbProbability" value="2"/><label for="radio2">2</label>
-                            <input type="radio" id="radio3" name="rbProbability" value="3"/><label for="radio3">3</label>
-                            <input type="radio" id="radio4" name="rbProbability" value="9999"/><label for="radio4">error</label>
-                        </div>                
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" class="align-left">
-                        <button id="btnViewAttributes">View attributes</button>
+        <form id="gc" class="pure-form pure-form-stacked">
+            <fieldset>
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-md-3-5">
+                        <label for="tbItemName">Item name</label>
+                        <input id="tbItemName" name="gc_name" type="text" class="pure-input-1 resetable"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-5">
+                        <label>X|Longitude</label>
+                        <input id="tbLongitude" name="tbLongitude" type="text" class="pure-input-1 resetable"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-5">
+                        <label>Y|Latitude</label>
+                        <input id="tbLatitude" name="tbLatitude" type="text" class="pure-input-1 resetable"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-2-5">
+                        <label>Linked persistent URI</label>
+                        <input id="tbLinkedPersistentURI" type="text" class="pure-input-1" readonly="true"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-5">
+                        <label>&nbsp;</label>
+                        <button id="btnClearPersistentURI">Clear URI</button>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-5">
+                        <label>Map resolution</label>
+                        <input id="tbMapResolution" type="text" class="pure-input-1" readonly="true"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-5">
+                        <label>Confidence (%)</label>
+                        <input id="tbConfidence" type="number" min="0" max="100" step="10" value="100" class="pure-input-1"/>
+                    </div>
+                    <div class="pure-u-1 pure-u-md-2-3">
+                        <label>More information</label>
+                        <button id="btnViewAttributes">View info</button>
                         <button id="btnViewImage">View image</button>
                         <button id="btnViewUrl">View link</button>
-                    </td>
-                    <td class="align-right">
+                    </div>
+                    <div class="pure-u-1 pure-u-md-1-3" style="text-align: right;">
+                        <label>&nbsp;</label>
                         <button id="btnCancel">Cancel</button>
                         <button id="btnSaveGeocoding">Save</button>
-                    </td>
-                </tr>
-            </table>
-            <input type="hidden" id="hdnImage" name="ds_image" class="resetable" value=""/>
-            <input type="hidden" id="hdnUrl" name="ds_url" class="resetable" value=""/>
-            <input type="hidden" id="hdnTableId" name="id" class="resetable" value=""/>
-            <input type="hidden" id="hdnAutoPkId" name="id" class="resetable" value=""/>
-            <input type="hidden" id="hdnFieldChanges" name="fieldChanges" class="resetable" value=""/>
-            <input type="hidden" id="hdnTableName" name="table" class="resetable" value=""/>
+                    </div>
+                </div>
+                <input type="hidden" id="hdnImage" name="ds_image" class="resetable" value=""/>
+                <input type="hidden" id="hdnUrl" name="ds_url" class="resetable" value=""/>
+                <input type="hidden" id="hdnTableId" name="id" class="resetable" value=""/>
+                <input type="hidden" id="hdnAutoPkId" name="id" class="resetable" value=""/>
+                <input type="hidden" id="hdnFieldChanges" name="fieldChanges" class="resetable" value=""/>
+                <input type="hidden" id="hdnTableName" name="table" class="resetable" value=""/>
+            </fieldset>
         </form>
     </div>
 </div>
