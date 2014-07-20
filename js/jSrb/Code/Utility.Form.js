aebbc612e930ae7331ff5d86f25ed064697f5b47
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-20 18:55:03 +0200

    Copied in functions from Utility.js

--- js/jSrb/Code/Utility.Form.js
+++ js/jSrb/Code/Utility.Form.js
@@ -1,3 +1,35 @@
+function resetGeocodingForm() {
+    jQuery("input.resetable, select.resetable", "form#gc").val("");
+    return true;
+}
+
+function getFieldChanges() {
+    var mFieldChanges = jQuery("#gc input#hdnFieldChanges").val();
+    if (mFieldChanges != '') {
+        return JSON.parse(jQuery("#gc input#hdnFieldChanges").val());
+    } else {
+        return JSON.parse('{}');
+    }
+}
+
+function getFieldChangesAsText() {
+    return jQuery("#gc input#hdnFieldChanges").val();
+}
+
+function setFieldChanges(pChangedFields) {
+    jQuery("#gc input#hdnFieldChanges").val(pChangedFields);
+    return true;
+}
+
+function hideGeocodingForm() {
+    jQuery("#gcFormContainer").css("display", "none");
+}
+
+
+function showGeocodingForm() {
+    jQuery("#gcFormContainer").css("display", "block");
+}
+
 /**
  * Open a popup window and show a URL
  * 
