aaf36061c68320001fb5a1d3de0674b5bbb82d35
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-21 12:55:48 +0200

    Removed redundant OpenLayers.Control.Click function

--- js/jSrb/Code/CustomControls.js
+++ js/jSrb/Code/CustomControls.js
@@ -1,27 +1,3 @@
 if (typeof(OpenLayers) !== 'undefined') {
-    OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-        defaultHandlerOptions: {
-            'single': true,
-            'double': false,
-            'pixelTolerance': 0,
-            'stopSingle': false,
-            'stopDouble': false
-        },
-        initialize: function(options) {
-            this.handlerOptions = OpenLayers.Util.extend(
-                    {}, this.defaultHandlerOptions
-                    );
-            OpenLayers.Control.prototype.initialize.apply(
-                    this, arguments
-                    );
-            this.handler = new OpenLayers.Handler.Click(
-                    this, {
-                'click': this.onClick
-            }, this.handlerOptions
-                    );
-        },
-        onClick: function(evt) {
-            handlerClickMap(evt.xy);
-        }
-    });
+    // Add any custom OpenLayers controls here
 }
