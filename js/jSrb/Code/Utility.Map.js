5542a8e9c62e883825da3bced8756a6499ab4936
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-07-21 13:27:40 +0200

    Added type-comparing error tolerance to addProposed and addExisting markers...

--- js/jSrb/Code/Utility.Map.js
+++ js/jSrb/Code/Utility.Map.js
@@ -1,30 +1,30 @@
 function addExistingIcon(pLonLat) {
-    if (currentMarker != null) {
+    if (currentMarker !== undefined && currentMarker !== null) {
         markers.removeMarker(currentMarker);
     }
-    var size = new OpenLayers.Size(27,27);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('images/white01.png',size,offset);
-    currentMarker = new OpenLayers.Marker(pLonLat,icon);
-    markers.addMarker(currentMarker);    
+    var size = new OpenLayers.Size(27, 27);
+    var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+    var icon = new OpenLayers.Icon('images/white01.png', size, offset);
+    currentMarker = new OpenLayers.Marker(pLonLat, icon);
+    markers.addMarker(currentMarker);
 }
 
 function addProposedIcon(pLonLat) {
-    if (proposedMarker != null) {
+    if (proposedMarker !== undefined && proposedMarker !== null) {
         markers.removeMarker(proposedMarker);
     }
-    var size = new OpenLayers.Size(27,27);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('images/green01.png',size,offset);
-    proposedMarker = new OpenLayers.Marker(pLonLat,icon);
+    var size = new OpenLayers.Size(27, 27);
+    var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+    var icon = new OpenLayers.Icon('images/green01.png', size, offset);
+    proposedMarker = new OpenLayers.Marker(pLonLat, icon);
     markers.addMarker(proposedMarker);
 }
 
 function addAlternateIcons(pLonLat) {
-    var size = new OpenLayers.Size(27,27);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('images/yellow01.png',size,offset);
-    var tmpMarker = new OpenLayers.Marker(pLonLat,icon);
+    var size = new OpenLayers.Size(27, 27);
+    var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+    var icon = new OpenLayers.Icon('images/yellow01.png', size, offset);
+    var tmpMarker = new OpenLayers.Marker(pLonLat, icon);
     alternateMarkers.push(tmpMarker);
     markers.addMarker(tmpMarker);
 }
@@ -35,5 +35,5 @@ function clearIcons() {
     }
     if (proposedMarker != null) {
         markers.removeMarker(proposedMarker);
-    }    
+    }
 }
