e5f74fb9e510d0077e1f2698ce8588c87004ecff
(Stein) Runar Bergheim <runarbe@gmail.com>
2015-09-30 21:40:43 +0200

    Updated source

--- latest/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$mVersion = file_get_contents(dirname(__FILE__) . "/version.txt");
-$mZipFileName = sprintf("locloudgc-%s.zip", $mVersion);
-header(sprintf('Content-Disposition: attachment; filename="%s"', $mZipFileName));
-?>
